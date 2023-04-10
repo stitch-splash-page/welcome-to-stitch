8265729d5b1fe6721899d5bdb68681a99f4b36c7
Jenny <jennyoh1216@gmail.com>
2023-04-10 19:44:58 -0400

    NavBar done

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,14 +1,21 @@
 import Image from 'next/image';
-import Logo from '../public/stitchLogo.jpg';
+import Logo from '../public/stitchLogo.png';
 import Link from 'next/link';
+// hamburger menu only shown on mobile screens. import react-icons library
+import { AiOutlineMenu } from 'react-icons/ai';
+import { useState } from 'react';
 
 //fixed at the top of the screen
 // on 2xl screens is doing a media query if the medium width is 1536px
 //tailwind is mobile responsive first
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const HandleNav = () => {
+    setMenuOpen(!menuOpen);
+  };
   return (
-    <nav className='fixed w-full h-24 shadow-xl bg-white'>
-      <ul class='left-header-nav flex justify-between items-center h-full w-full px-4 2xl:px-16'>
+    <nav className='flex flex-row fixed w-full h-24 shadow-xl bg-white'>
+      <div className='left-header-nav flex justify-between items-center h-full w-full px-6 2xl:px-16'>
         <Link href='/'>
           <Image
             //importing the logo
@@ -20,15 +27,28 @@ const Navbar = () => {
             priority
           />
         </Link>
-      </ul>
-      <div>
-        <ul class='right-header-nav'>
-          <a href='/about-us'>About Us</a>
-          <a href=''>Version</a>
-          <a href='/'>Home</a>
-          <a href='/github'>GitHub</a>
-          <a href='npm'>npm</a>
-        </ul>
+
+        <div className='hidden sm:flex'>
+          <ul className='hidden sm:flex'>
+            <Link href='/'>
+              <li className='ml-0 uppercase hover:border-b text-xl'>Home</li>
+            </Link>
+            <Link href='/about-us'>
+              <li className='ml-10 uppercase hover:border-b text-xl'>
+                About Us
+              </li>
+            </Link>
+            <Link href='/github'>
+              <li className='ml-10 uppercase hover:border-b text-xl'>GitHub</li>
+            </Link>
+            <Link href='/npm'>
+              <li className='ml-10 uppercase hover:border-b text-xl'>npm</li>
+            </Link>
+          </ul>
+        </div>
+        <div onClick={HandleNav} className='sm:hidden cursor-pointer pl-24'>
+          <AiOutlineMenu size={25} />
+        </div>
       </div>
     </nav>
   );
