e4c6cabe3988ebab01375c720e69bf79f1dabce5
Jenny <jennyoh1216@gmail.com>
2023-04-17 19:15:35 -0400

    hero component, Text Containter and edited tailwind

---
+++ components/AboutStitch.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Image from "next/image";
+import AboutImg from "../public/stitchImg.png";
+
+const About = () => {
+    return (
+    <section className=" font-sans px-5 py-20 border-solid border-9 border-amber-100  bg-white ">
+        <div className=" container mx-auto grid md:grid-cols-2 items-center justify-center md:justify-between ">
+        <div className="about-info">
+            <h1 className="text-7xl font-bold mb-5 border-b-[5px] w-[250px] border-rose-500 pb-2">
+            About Stitch
+            </h1>
+            <p className="pb-5 text-2xl">
+                Looking to migrate from a traditional REST API to GraphQL but don't want to deal with the complexity and tedium of implementation? 
+            </p>
+            <p className="pb-5 text-2xl">
+                This is where  <span className="font-bold">Stich</span>  comes into play. 
+            </p>
+        </div>
+
+        <div className="about-img flex justify-end">
+            <Image
+        src={AboutImg}
+        alt="Picture of the author"
+        width={500}
+        height={500}
+    />
+        </div>
+        </div>
+    </section>
+    );
+};
+
+export default About;
---
+++ components/Text-Container.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Link from "next/link";
+
+const TextContainer = () => {
+    return (
+        <section className="px-5 py-20 bg-gradient-to-r from-rose-400 to-red-500" id="Text-Container">
+      <div className="text-center md:w-[60%] mx-auto text-white">
+        <h2 className="text-5xl font-bold mb-5 border-b-[5px] mx-auto border-rose-500  pb-2">
+          Simple and Customizable
+        </h2>
+        <p className="pb-5 text-2xl"> This lightweight tool allows you to quickly and easily generate GraphQL schemas from your existing RESTful endpoints, simplifying the process of creating a GraphQL backend and saving you time and effort.</p>
+        <div className="pt-5">
+            <Link className="bg-accent rounded border-2 border-white font-ui-sans-serif text-white px-6 py-3 hover:bg-transparent " href='/'>
+            Learn More
+        </Link>
+        </div>
+      </div>
+      
+    </section>
+    )
+}
+
+export default TextContainer;
