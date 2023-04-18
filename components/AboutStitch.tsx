d416e5235d1ca62c52e414090da34398a52cdd9a
Jenny <jennyoh1216@gmail.com>
2023-04-18 15:23:42 -0400

    added svg icons Linkedin/Github for creators Contact us, linked

--- components/AboutStitch.tsx
+++ components/AboutStitch.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import Image from "next/image";
-import AboutImg from "../public/stitchImg.png";
 
 const About = () => {
     return (
@@ -17,18 +15,30 @@ const About = () => {
                 This is where  <span className="font-bold">Stich</span>  comes into play. 
             </p>
         </div>
-
         <div className="about-img flex justify-end">
-            <Image
+            {/* <Image
         src={AboutImg}
         alt="Picture of the author"
         width={500}
         height={500}
-    />
+    /> */}
+     <div className="max-w-7xl mx-auto">
+    <div className="relative group">
+      <div className="absolute -inset-1 bg-gradient-to-r from-purple-600 to-pink-600 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
+      <div className="relative px-7 py-6 bg-white ring-1 ring-gray-900/5 rounded-lg leading-none flex items-top justify-start space-x-6">
+        <svg className="w-8 h-8 text-purple-600" fill="none" viewBox="0 0 24 24">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M6.75 6.75C6.75 5.64543 7.64543 4.75 8.75 4.75H15.25C16.3546 4.75 17.25 5.64543 17.25 6.75V19.25L12 14.75L6.75 19.25V6.75Z"></path>
+        </svg>
+        <div className="space-y-2">
+          <p className="text-slate-800">Medium</p>
+          <a href="/" className="block text-indigo-400 group-hover:text-slate-800 transition duration-200" target="_blank">Read Article →</a>
+      </div>
+    </div>
+  </div>
+</div>
         </div>
         </div>
     </section>
     );
 };
-
 export default About;
--- components/layouts.tsx
+++ components/layouts.tsx
@@ -1,6 +1,6 @@
-import Navbar from './Navbar'
-import Footer from './Footer'
-import React, { PropsWithChildren } from "react"
+import Navbar from './Navbar';
+import Footer from './Footer';
+import React, { PropsWithChildren } from "react";
 
 //The main element is also rendered with the children prop spread onto it. This allows the child components of the Layout component to be rendered inside the main element.
 //Layout component is intended to be used as a wrapper
@@ -12,6 +12,5 @@ export default function Layout({ children }: PropsWithChildren) {
       <main>{children}</main>
       <Footer />
     </>
-    
-  )
-}
+  );
+};
